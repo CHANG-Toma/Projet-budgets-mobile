dbd9720ae4e8c0547ed5cd8aa348cb0e5c01fe4f
CHANG-Toma <toma11chang@gmail.com>
2025-11-03 16:37:05 +0100

    feat(dashboard): adding expense form

--- obj/Debug/net9.0-windows10.0.19041.0/win10-x64/Projet Budget M1.AssemblyInfo.cs
+++ obj/Debug/net9.0-windows10.0.19041.0/win10-x64/Projet Budget M1.AssemblyInfo.cs
@@ -17,7 +17,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Projet Budget M1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+7cbd528e501816cd037f2462c854aaf969d4f1d4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+a92d2951efec3cb5202ca8ed9f3a67911a7badb5")]
 [assembly: System.Reflection.AssemblyProductAttribute("Projet Budget M1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Projet Budget M1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
